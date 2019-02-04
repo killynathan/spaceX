60ad680690849ecefc9fa2aa15b2d57190bf56b9
nathan kou <nathankou12@gmail.com>
2019-02-03 23:45:30 -0800

    first graphql app

---
+++ public/precache-manifest.07bb57d23d6b9cad39cbbb015f3565b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c458fae08385518449f0",
+    "url": "/static/js/main.c458fae0.chunk.js"
+  },
+  {
+    "revision": "c49773e3f8e319655164",
+    "url": "/static/js/1.c49773e3.chunk.js"
+  },
+  {
+    "revision": "c458fae08385518449f0",
+    "url": "/static/css/main.947a6152.chunk.css"
+  },
+  {
+    "revision": "7dccbe7c7e7d1adba597bada9854c922",
+    "url": "/index.html"
+  }
+];
